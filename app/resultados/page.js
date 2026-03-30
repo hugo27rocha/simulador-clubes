01440dcc2809e73e39e49c9bf888823fd6c9a192
hugo27rocha <hugo27rocha@gmail.com>
2026-03-30 10:23:00 +0100

    fix vercel build resultados

--- app/resultados/page.js
+++ app/resultados/page.js
@@ -1,7 +1,10 @@
 "use client"
 
+export const dynamic = "force-dynamic"
+
 import { useEffect, useState } from "react"
 import { useRouter, useSearchParams } from "next/navigation"
+
 
 export default function Resultados() {
   const router = useRouter()
